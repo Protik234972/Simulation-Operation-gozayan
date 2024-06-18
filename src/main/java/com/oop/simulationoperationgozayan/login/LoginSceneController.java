65d3058e8e01b5a0631e61292c1f7c7e562bc7c8
Protik234972 <baruaprotik234972@gmail.com>
2024-06-18 17:45:10 +0600

    LoginValidationAndVerification.java file added. Scene change according to user type selected

--- src/main/java/com/oop/simulationoperationgozayan/login/LoginSceneController.java
+++ src/main/java/com/oop/simulationoperationgozayan/login/LoginSceneController.java
@@ -43,7 +43,40 @@ public class LoginSceneController {
     }
 
     @javafx.fxml.FXML
-    public void signInButtonOnClicked(ActionEvent actionEvent) {
+    public void signInButtonOnClicked(ActionEvent actionEvent) throws IOException {
+        String emailOrUserId = emailOrUserIdTextField.getText();
+        String password = passwordTextField.getText();
+        String userType = userTypeComboBox.getValue();
+
+        LoginValidationAndVerification loginValidationAndVerification = new LoginValidationAndVerification();
+        if (loginValidationAndVerification.validateEmailOrId(emailOrUserId) && loginValidationAndVerification.validatePassword(password)) {
+            Parent dashboardScene = switch (userType) {
+                case "Customer" ->
+                        FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/oop/simulationoperationgozayan/protik/customer/customerDashboardScene.fxml")));
+                case "Accounts & Finance Officer" ->
+                        FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/oop/simulationoperationgozayan/protik/accounts_and_finance_officer/accountOfficerDashboardScene.fxml")));
+                case "Customer Service Executive" ->
+                        FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/oop/simulationoperationgozayan/plabon/customer_service_executive/customerServiceExecutiveDashboardScene.fxml")));
+                case "Operation Manager" ->
+                        FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/oop/simulationoperationgozayan/plabon/operation_manager/operationManagerDashboardScene.fxml")));
+                case "Admin" ->
+                        FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/oop/simulationoperationgozayan/amanta/admin/adminDashboardScene.fxml")));
+                case "HRM" ->
+                        FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/oop/simulationoperationgozayan/amanta/hrm/hrmDashboardScene.fxml")));
+                case "Hotel Manager" ->
+                        FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/oop/simulationoperationgozayan/toushia/hotelManager/hotelManagerDashboardScene.fxml")));
+                case "Tour Agent" ->
+                        FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/oop/simulationoperationgozayan/toushia/tourAgent/tourAgentDashboardScene.fxml")));
+                default -> throw new IllegalArgumentException("Invalid user type: " + userType);
+            };
+            Scene scene = new Scene(dashboardScene);
+            Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+            window.setTitle(userType + " Dashboard");
+            window.setScene(scene);
+            window.show();
+        } else {
+            System.out.println("Invalid Email or User ID or Password");
+        }
     }
 
     @javafx.fxml.FXML
